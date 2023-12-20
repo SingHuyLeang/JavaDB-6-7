3ec7517dc86455f6edebfc4898da7eb462358fa5
SingHuyLeang <leang6525@gmail.com>
2023-12-20 18:24:20 +0700

    test

--- src/Views/Home/Home.java
+++ src/Views/Home/Home.java
@@ -4,11 +4,21 @@ import java.awt.Image;
 import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class Home extends javax.swing.JFrame {
     public Home() {
         initComponents();
     }
+    public void clearAllField(){
+        txtName.setText("");
+        txtQty.setText("");
+        txtPrice.setText("");
+        cbDiscount.setSelectedIndex(0);
+        txtImage.setText("");
+        lbImage.setIcon(null);
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -16,21 +26,22 @@ public class Home extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        table = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtName = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        txtQty = new javax.swing.JTextField();
         lbImage = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        txtImage = new javax.swing.JTextField();
         btnImage = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnClear = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        cbDiscount = new javax.swing.JComboBox<>();
+        txtPrice = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1000, 600));
@@ -43,7 +54,7 @@ public class Home extends javax.swing.JFrame {
         jLabel1.setText("SHOP");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 988, 78));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        table.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -51,7 +62,7 @@ public class Home extends javax.swing.JFrame {
                 "Id", "Name", "Quantity", "Price", "Total", "Discount", "Payment", "Image", "Date"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(table);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 910, 260));
 
@@ -61,9 +72,9 @@ public class Home extends javax.swing.JFrame {
         jLabel2.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 370, 90, 30));
 
-        jTextField1.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jTextField1.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 370, 180, 30));
+        txtName.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        txtName.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
+        jPanel1.add(txtName, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 370, 180, 30));
 
         jLabel3.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -71,18 +82,18 @@ public class Home extends javax.swing.JFrame {
         jLabel3.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 90, 30));
 
-        jTextField2.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jTextField2.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
-        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 420, 180, 30));
+        txtQty.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        txtQty.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
+        jPanel1.add(txtQty, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 420, 180, 30));
 
         lbImage.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
         lbImage.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         lbImage.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel1.add(lbImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 360, 130, 140));
 
-        jTextField3.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jTextField3.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
-        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 370, 180, 30));
+        txtImage.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        txtImage.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
+        jPanel1.add(txtImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 557, 0, 0));
 
         btnImage.setBackground(new java.awt.Color(204, 255, 204));
         btnImage.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
@@ -94,25 +105,35 @@ public class Home extends javax.swing.JFrame {
         });
         jPanel1.add(btnImage, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 530, 130, -1));
 
-        jButton2.setBackground(new java.awt.Color(204, 255, 204));
-        jButton2.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jButton2.setText("Add");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 530, 130, -1));
+        btnAdd.setBackground(new java.awt.Color(204, 255, 204));
+        btnAdd.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        btnAdd.setText("Add");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnAdd, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 530, 130, -1));
 
-        jButton3.setBackground(new java.awt.Color(255, 255, 204));
-        jButton3.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jButton3.setText("Update");
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 530, 130, -1));
+        btnUpdate.setBackground(new java.awt.Color(255, 255, 204));
+        btnUpdate.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        btnUpdate.setText("Update");
+        jPanel1.add(btnUpdate, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 530, 130, -1));
 
-        jButton4.setBackground(new java.awt.Color(255, 153, 153));
-        jButton4.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jButton4.setText("Delete");
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 530, 130, -1));
+        btnDelete.setBackground(new java.awt.Color(255, 153, 153));
+        btnDelete.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        btnDelete.setText("Delete");
+        jPanel1.add(btnDelete, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 530, 130, -1));
 
-        jButton5.setBackground(new java.awt.Color(255, 204, 204));
-        jButton5.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jButton5.setText("Clear");
-        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 530, 130, -1));
+        btnClear.setBackground(new java.awt.Color(255, 204, 204));
+        btnClear.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        btnClear.setText("Clear");
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 530, 130, -1));
 
         jLabel5.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -126,19 +147,20 @@ public class Home extends javax.swing.JFrame {
         jLabel6.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
         jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 420, 90, 30));
 
-        jComboBox1.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "0 %", "10 %", "20 %", "30 %", "40 %", "50 %" }));
-        jComboBox1.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
-        jPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 420, 180, 30));
+        cbDiscount.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        cbDiscount.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "0 %", "10 %", "20 %", "30 %", "40 %", "50 %" }));
+        cbDiscount.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
+        jPanel1.add(cbDiscount, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 420, 180, 30));
+
+        txtPrice.setFont(new java.awt.Font("Barlow", 0, 18)); // NOI18N
+        txtPrice.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(100, 100, 100)));
+        jPanel1.add(txtPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 370, 180, 30));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1006, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -157,7 +179,30 @@ public class Home extends javax.swing.JFrame {
         ImageIcon imgIcon = new ImageIcon(fileName);
         Image image = imgIcon.getImage().getScaledInstance(lbImage.getWidth(), lbImage.getHeight(), Image.SCALE_SMOOTH);
         lbImage.setIcon(new ImageIcon(image));
+        txtImage.setText(fileName);
     }//GEN-LAST:event_btnImageActionPerformed
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        String name = txtName.getText();
+        String qty  = txtQty.getText();
+        String price = txtPrice.getText();
+        String discount = cbDiscount.getSelectedItem().toString();
+        String image = txtImage.getText();
+        
+        if (!name.isEmpty() && !qty.isEmpty() && !price.isEmpty() && !discount.isEmpty() && !image.isEmpty()) {
+            System.out.println("Name     : "+name);
+            System.out.println("Qty      : "+qty);
+            System.out.println("Price    : "+price);
+            System.out.println("Discount : "+discount);
+            clearAllField();
+        }else{
+            JOptionPane.showMessageDialog(this, "Please enter all field", "Warining", JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        clearAllField();
+    }//GEN-LAST:event_btnClearActionPerformed
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -167,12 +212,12 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnDelete;
     private javax.swing.JButton btnImage;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JComboBox<String> cbDiscount;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -180,10 +225,11 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     private javax.swing.JLabel lbImage;
+    private javax.swing.JTable table;
+    private javax.swing.JTextField txtImage;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JTextField txtPrice;
+    private javax.swing.JTextField txtQty;
     // End of variables declaration//GEN-END:variables
 }
